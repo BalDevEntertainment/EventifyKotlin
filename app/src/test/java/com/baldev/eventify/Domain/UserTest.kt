fdd900090119faf2740ed89e80f2933ba5b1be89
Ariel <galiano.ariel@gmail.com>
2017-11-16 02:01:09 -0300

    Improved text readabilityK

--- app/src/test/java/com/baldev/eventify/Domain/UserTest.kt
+++ app/src/test/java/com/baldev/eventify/Domain/UserTest.kt
@@ -1,17 +1,19 @@
 package com.baldev.eventify.Domain
 
 import com.baldev.eventify.Domain.User.CreateUser
+import com.baldev.eventify.Domain.User.User
 import io.kotlintest.specs.BehaviorSpec
 
 class UserTest : BehaviorSpec({
     Given("Some user name") {
         val userName = "UserName"
-        val users = provideUsers()
         When("Create user") {
-            val user = CreateUser(userName, users)()
+            val createUser = CreateUser(userName, provideUsers())()
             Then("User is created") {
-                user.test().assertValue { it.userName == userName }
+                createUser.test().assertValue { hasUsername(it, userName) }
             }
         }
     }
 })
+
+private fun hasUsername(it: User, userName: String) = it.userName == userName
